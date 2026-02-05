d5aad211590d5f1efe2a551f301919d9f978ba61
Fernando Duarte <fernando.dsouza@outlook.com>
2026-02-05 16:22:15 -0300

    Primeiro commit.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import TopazToolsCliente from "@/app/components/topaz-tools-cliente";
+// import TopazToolsCliente from "@/app/components/topaz-tools-cliente";
 
 export default function TopazToolsPage() {
     return <>
-        <main className="min-h-screen bg-[var(--background)] text-[var(--foreground)]">
-            <TopazToolsCliente/>
-        </main>
+        <h1>HOME FUNCIONANDO</h1>
+        {/*<main className="min-h-screen bg-[var(--background)] text-[var(--foreground)]">*/}
+        {/*    <TopazToolsCliente/>*/}
+        {/*</main>*/}
     </>
 }
